c11562d72cb4e8eb75b8467f212a5ff567271d39
Arksine <arksine.code@gmail.com>
2017-10-01 12:57:04 -0400

    Setup JNA

---
+++ libusbtv/src/main/java/com/arksine/libusbtv/UsbtvDriver.java
@@ -0,0 +1,15 @@
+package com.arksine.libusbtv;
+
+import android.hardware.usb.UsbDevice;
+
+/**
+ * Created by Eric on 10/1/2017.
+ */
+
+public class UsbtvDriver {
+    // BuildTest
+    public UsbtvDriver(UsbDevice usbtvDevice) {
+
+    }
+
+}
